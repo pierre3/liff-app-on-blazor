f3ae69d868b28fe9226dadf3ef405ffd50496bee
pierre3 <pierre3@outlook.com>
2019-06-15 13:24:28 +0900

    add docs

---
+++ docs/script/liffExtension.js
@@ -0,0 +1,12 @@
+﻿window.liffExt = {
+    init: function (context) {
+        liff.init(
+            function (data) {
+                context.invokeMethod('LiffInitSuccess', data);
+            },
+            function (error) {
+                context.invokeMethod('LiffInitError', error);
+            }
+        );
+    }
+};
