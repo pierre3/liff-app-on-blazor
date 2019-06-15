71279ba139393ebc4ea5d11112cce9055aa68f18
pierre3 <pierre3@outlook.com>
2019-06-15 22:19:47 +0900

    _

---
+++ LiffAppOnBlazor/CamelCaseJsonSerializerSettings.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiffAppOnBlazor
+{
+    public class CamelCaseJsonSerializerSettings : JsonSerializerSettings
+    {
+        public CamelCaseJsonSerializerSettings()
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver();
+            Converters.Add(new StringEnumConverter()
+            {
+                NamingStrategy = new CamelCaseNamingStrategy()
+            });
+        }
+    }
+}
