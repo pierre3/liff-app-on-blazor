2b3bb5327e5ce6d802599647f77ebce62b9fcc90
pierre3 <pierre3@outlook.com>
2019-06-16 09:32:11 +0900

    _

--- LiffAppOnBlazor/wwwroot/script/liffExtension.js
+++ LiffAppOnBlazor/wwwroot/script/liffExtension.js
@@ -2,7 +2,7 @@
     init: function (context) {
         liff.init(
             function (data) {
-                context.invokeMethod('LiffInitSuccess', JSON.stringify(data));
+                context.invokeMethod('LiffInitSuccess', data);
             },
             function (error) {
 
