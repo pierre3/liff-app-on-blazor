d4956f76c088ab1e443a385112ae7d4d17fc4322
pierre3 <pierre3@outlook.com>
2019-06-25 14:22:51 +0900

    changed LiffSdk.Blazor liblary type  to blazorlib

---
+++ docs/_content/LiffSdk.Blazor/liffInterop.js
@@ -0,0 +1,19 @@
+﻿window.liffInterop = {
+    init: function (dotNetRef) {
+        liff.init(
+            function (data) {
+                dotNetRef.invokeMethod('OnInitSuccess', JSON.stringify(data));
+            },
+            function (error) {
+                dotNetRef.invokeMethod('OnInitError', JSON.stringify({
+                    code: error.code,
+                    message: error.message,
+                    stack: error.stack
+                }));
+            }
+        );
+    },
+    alert: function (message) {
+        alert(message);
+    }
+};
